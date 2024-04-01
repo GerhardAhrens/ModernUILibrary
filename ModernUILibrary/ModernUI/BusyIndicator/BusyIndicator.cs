b3b5d68643b0c71b1639c04c3996cce0dbf6de44
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2024-04-01 10:10:30 +0200

    Add BusyIndicator

---
+++ ModernUILibrary/ModernUI/BusyIndicator/BusyIndicator.cs
@@ -0,0 +1,51 @@
+﻿namespace ModernIU.Controls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    public class BusyIndicator : Control
+    {
+        public static readonly DependencyProperty IsBusyProperty;
+        public static readonly DependencyProperty TextProperty;
+        public static readonly DependencyProperty LoadingColorProperty;
+
+        public bool IsBusy
+        {
+            get { return (bool)GetValue(IsBusyProperty); }
+            set { SetValue(IsBusyProperty, value); }
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public Brush LoadingColor
+        {
+            get { return (Brush)GetValue(LoadingColorProperty); }
+            set { SetValue(LoadingColorProperty, value); }
+        }
+
+        private static void OnIsBusyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            BusyIndicator indicator = d as BusyIndicator;
+            indicator.Visibility = indicator.IsBusy ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        static BusyIndicator()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BusyIndicator), new FrameworkPropertyMetadata(typeof(BusyIndicator)));
+            BusyIndicator.IsBusyProperty = DependencyProperty.Register("IsBusy", typeof(bool), typeof(BusyIndicator), new PropertyMetadata(false, OnIsBusyChangedCallback));
+            BusyIndicator.TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(BusyIndicator), new PropertyMetadata("加载中..."));
+            BusyIndicator.LoadingColorProperty = DependencyProperty.Register("LoadingColor", typeof(Brush), typeof(BusyIndicator), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(0, 122, 204))));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            this.Visibility = this.IsBusy ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
---
+++ ModernUILibrary/ModernUI/BusyIndicator/BusyIndicatorAdorner.cs
@@ -0,0 +1,48 @@
+﻿namespace ModernIU.Controls
+{
+    using System.Windows;
+    using System.Windows.Documents;
+    using System.Windows.Media;
+
+    using ModernIU.Base;
+
+    public class BusyIndicatorAdorner : Adorner
+    {
+        private VisualCollection _visuals;
+        private BusyIndicator busyIndicator;
+
+        public static bool GetIsOpen(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsOpenProperty);
+        }
+
+        public static void SetIsOpen(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsOpenProperty, value);
+        }
+
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.RegisterAttached("IsOpen", typeof(bool), typeof(BusyIndicatorAdorner)
+                , new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault | FrameworkPropertyMetadataOptions.Journal, BusyIndicatorAdorner.IsOpenCallback, CoerceIsOpen, true, System.Windows.Data.UpdateSourceTrigger.PropertyChanged));
+
+        private static void IsOpenCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            BusyIndicatorAdorner adorner = UIElementEx.GetAdorner<BusyIndicatorAdorner>(d);
+            if(adorner == null)
+            {
+                return;
+            }
+        }
+
+        private static object CoerceIsOpen(DependencyObject d, object baseValue)
+        {
+            if (baseValue == null)
+                return false;
+            return baseValue;
+        }
+
+        public BusyIndicatorAdorner(UIElement adornedElement) : base(adornedElement)
+        {
+        }
+    }
+}
