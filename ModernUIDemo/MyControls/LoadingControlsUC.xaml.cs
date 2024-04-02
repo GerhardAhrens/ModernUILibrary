0f0e32a8e2b5fd6fa49d344cf7ba99ec827a0c61
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2024-04-02 19:44:47 +0200

    *

---
+++ ModernUIDemo/MyControls/LoadingControlsUC.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ModernUIDemo.MyControls
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaktionslogik für LoadingControlsUC.xaml
+    /// </summary>
+    public partial class LoadingControlsUC : UserControl
+    {
+        public LoadingControlsUC()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
