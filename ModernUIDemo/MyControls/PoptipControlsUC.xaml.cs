afe8c45e94748ee3eadedcaa5ffc4a2f5bbdc7a6
Gerhard Ahrens <gerhard.ahrens@pta.de>
2024-04-17 17:40:36 +0200

    *

---
+++ ModernUIDemo/MyControls/PoptipControlsUC.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ModernUIDemo.MyControls
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaktionslogik für PoptipControlsUC.xaml
+    /// </summary>
+    public partial class PoptipControlsUC : UserControl
+    {
+        public PoptipControlsUC()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
