4f2ac0b3c576519b318406b91014b3990c0275b0
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2024-04-18 20:52:32 +0200

    *

---
+++ ModernUIDemo/MyControls/MessageBoxControlsUC.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace ModernUIDemo.MyControls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using ModernIU.Base;
+
+    using ModernIU.Controls;
+
+    /// <summary>
+    /// Interaktionslogik für MessageBoxControlsUC.xaml
+    /// </summary>
+    public partial class MessageBoxControlsUC : UserControl
+    {
+        public MessageBoxControlsUC()
+        {
+            this.InitializeComponent();
+        }
+
+        private void FlatButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            switch (btn.Content.ToString())
+            {
+                case "Info":
+                    if (MMessageBox.Show("Element", "Bildunterschrift", EnumPromptType.Error) == MessageBoxResult.OK)
+                    {
+                        MessageBox.Show("Klicken Sie auf OK.");
+                    }
+                    break;
+                case "Error":
+                    MMessageBox.Show(Window.GetWindow(this), "Sie verwenden derzeit eine zu niedrige Browserversion, die dazu führen kann, dass ein Teil der Funktion des Produkts nicht normal genutzt werden kann", "", MessageBoxButton.YesNoCancel, EnumPromptType.Warn);
+                    break;
+                case "Warn":
+                    MMessageBox.Show("Element", EnumPromptType.Warn);
+                    break;
+                case "Success":
+                    MMessageBox.Show("Element", EnumPromptType.Success);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ ModernUILibrary/Converters/StringIsEmptyConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace ModernIU.Converters
+{
+    using System.Windows.Data;
+
+    public class StringIsEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return string.IsNullOrEmpty(System.Convert.ToString(value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
