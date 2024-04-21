a4ef2affff71ac50a063621fa6d455178c91a095
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2024-04-21 17:07:52 +0200

    *

--- ModernUIDemo/MainWindow.xaml.cs
+++ ModernUIDemo/MainWindow.xaml.cs
@@ -4,6 +4,9 @@
     using System.ComponentModel;
     using System.Runtime.CompilerServices;
     using System.Windows;
+    using System.Windows.Data;
+
+    using Microsoft.VisualBasic;
 
     using ModernUIDemo.Core;
     using ModernUIDemo.Model;
@@ -20,18 +23,36 @@
 
             WeakEventManager<Window, RoutedEventArgs>.AddHandler(this, "Loaded", this.OnLoaded);
 
-            this.TabControlSource.Value = new ObservableCollection<TabControlItem>();
-            this.TabControlSource.Value.Add(new TabControlItem("Darstellung", true));
-            this.TabControlSource.Value.Add(new TabControlItem("Icon (PathGeometry)", new IconsControlsUC()));
-            this.TabControlSource.Value.Add(new TabControlItem("Farben", new ColorControlsUC()));
-            this.TabControlSource.Value.Add(new TabControlItem("Farben", new ColorControlsUC()));
-            this.TabControlSource.Value.Add(new TabControlItem("Eingabe", true));
-            this.TabControlSource.Value.Add(new TabControlItem("TextBox (String) Controls", new TextBoxStringControlsUC()));
+            List<TabControlItem> tabItemSource = new List<TabControlItem>();
+            tabItemSource.Add(new TabControlItem("Darstellung", true));
+            tabItemSource.Add(new TabControlItem("Icon (PathGeometry)", new IconsControlsUC()));
+            tabItemSource.Add(new TabControlItem("Farben", new ColorControlsUC()));
+            tabItemSource.Add(new TabControlItem("Farben", new ColorControlsUC()));
+            tabItemSource.Add(new TabControlItem("Eingabe", true));
+            tabItemSource.Add(new TabControlItem("TextBox (String) Controls", new TextBoxStringControlsUC()));
+            tabItemSource.Add(new TabControlItem("TextBox (Numeric) Controls", new TextBoxNumericControlsUC()));
+            tabItemSource.Add(new TabControlItem("ListBox Controls", new ListBoxControlsUC()));
+            tabItemSource.Add(new TabControlItem("Button Controls", new ButtonControlsUC()));
+            tabItemSource.Add(new TabControlItem("DropDownButton Controls", new DropDownButtonControlsUC()));
+            tabItemSource.Add(new TabControlItem("RadioButton Controls", new RadioButtonControlsUC()));
+            tabItemSource.Add(new TabControlItem("NumericUpDown Controls", new NumericUpDownControlsUC()));
+            tabItemSource.Add(new TabControlItem("View, Loading", true));
+            tabItemSource.Add(new TabControlItem("Badges Controls", new BadgesControlsUC()));
+            tabItemSource.Add(new TabControlItem("BusyIndicator Controls", new BusyIndicatorControlsUC()));
+            tabItemSource.Add(new TabControlItem("Loading Animation Controls", new LoadingControlsUC()));
+            tabItemSource.Add(new TabControlItem($"MessageBox, NoticeMessage\nWindow", true));
+            tabItemSource.Add(new TabControlItem("NoticeMessage Controls", new NoticeMessageControlsUC()));
+            tabItemSource.Add(new TabControlItem("MessageBox Window", new MessageBoxControlsUC()));
+            tabItemSource.Add(new TabControlItem("PopUp Window", new PopUpControlsUC()));
+            tabItemSource.Add(new TabControlItem("Tooltip Controls", new PopUpControlsUC()));
+
+            this.TabControlSource.Value = CollectionViewSource.GetDefaultView(tabItemSource);
+
 
             this.DataContext = this;
         }
 
-        public XamlProperty<ObservableCollection<TabControlItem>> TabControlSource { get; set; } = XamlProperty.Set<ObservableCollection<TabControlItem>>();
+        public XamlProperty<ICollectionView> TabControlSource { get; set; } = XamlProperty.Set<ICollectionView>();
         public XamlProperty<List<string>> MComboBoxSource { get; set; } = XamlProperty.Set<List<string>>();
         public XamlProperty<string> MComboBoxSourceSelectedItem { get; set; } = XamlProperty.Set<string>();
 
