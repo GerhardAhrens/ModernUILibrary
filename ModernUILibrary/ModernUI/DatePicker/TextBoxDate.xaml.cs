207a8439ef810da56f2edf82f2b96251e9cc18f4
Gerhard Ahrens <gerhard.ahrens@pta.de>
2024-05-14 16:34:04 +0200

    *

---
+++ ModernUILibrary/ModernUI/DatePicker/TextBoxDate.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace ModernIU.Controls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    using ModernIU.Base;
+
+    /// <summary>
+    /// Interaktionslogik für TextBoxDate.xaml
+    /// </summary>
+    public partial class TextBoxDate : UserControl
+    {
+        public TextBoxDate()
+        {
+            this.InitializeComponent();
+
+            this.FontSize = ControlBase.FontSize;
+            this.FontFamily = ControlBase.FontFamily;
+            this.HorizontalContentAlignment = HorizontalAlignment.Left;
+            this.VerticalContentAlignment = VerticalAlignment.Center;
+            this.Margin = new Thickness(2);
+            this.Focusable = true;
+
+            this.DataContext = this;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            this.BorderBrush = System.Windows.Media.Brushes.Green;
+            this.BorderThickness = new Thickness(1);
+
+        }
+    }
+}
