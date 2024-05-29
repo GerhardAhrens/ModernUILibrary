69a19ba92584bae7299c9fb2cf8cb2332cff4584
Gerhard Ahrens <gerhard.ahrens@pta.de>
2024-05-29 11:30:11 +0200

    *

---
+++ ModernUIDemo/MyControls/NotificationServiceControlsUC.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace ModernUIDemo.MyControls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaktionslogik für NotificationServiceControlsUC.xaml
+    /// </summary>
+    public partial class NotificationServiceControlsUC : UserControl
+    {
+        public NotificationServiceControlsUC()
+        {
+            this.InitializeComponent();
+            WeakEventManager<UserControl, RoutedEventArgs>.AddHandler(this, "Loaded", this.OnLoaded);
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
