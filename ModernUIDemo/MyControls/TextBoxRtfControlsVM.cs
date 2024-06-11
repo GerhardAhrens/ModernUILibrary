056e6f025917aae61efc2e157d3078ed161e8dc5
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2024-06-11 20:44:00 +0200

    *

---
+++ ModernUIDemo/MyControls/TextBoxRtfControlsVM.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ModernUIDemo.MyControls
+{
+    using ModernUIDemo.Core;
+
+    public class TextBoxRtfLControlsVM : BindableBase
+    {
+        private string _DocumentTextRTF;
+
+        public TextBoxRtfLControlsVM()
+        {
+            this.DocumentTextRTF = "Keine Nachricht";
+        }
+
+        public string DocumentTextRTF
+        {
+            get { return _DocumentTextRTF; }
+            set 
+            { 
+                this.SetProperty(ref this._DocumentTextRTF, value); 
+            }
+        }
+    }
+}
