01e6ab47d7476ef6d7d9351a04c9c38a06c707ac
Gerhard Ahrens <gerhard.ahrens@pta.de>
2024-11-05 09:41:16 +0100

    *

---
+++ ModernUILibrary/Converters/WordAngleConverter.cs
@@ -0,0 +1,34 @@
+﻿namespace ModernIU.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class WordAngleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string selected = "00";
+            int hour = 0;
+
+            if(int.TryParse(System.Convert.ToString(value), out hour))
+            {
+                if(hour < 10)
+                {
+                    selected = "0" + hour;
+                }
+                else
+                {
+                    selected = System.Convert.ToString(hour);
+                }
+            }
+            
+            return selected;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
