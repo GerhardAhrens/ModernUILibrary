c63ae44770b7d8e9a3739c9070c7529b27bef9a5
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2024-11-13 19:59:57 +0100

    *

--- ModernUIDemo/MyControls/ComboBoxControlsUC.xaml.cs
+++ ModernUIDemo/MyControls/ComboBoxControlsUC.xaml.cs
@@ -1,12 +1,17 @@
 ﻿namespace ModernUIDemo.MyControls
 {
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
     using System.Windows;
     using System.Windows.Controls;
+    using ModernUIDemo.Core;
+
+    using static ModernUIDemo.MyControls.ListBoxControlsUC;
 
     /// <summary>
     /// Interaktionslogik für ComboBoxControlsUC.xaml
     /// </summary>
-    public partial class ComboBoxControlsUC : UserControl
+    public partial class ComboBoxControlsUC : UserControl, INotifyPropertyChanged
     {
         public ComboBoxControlsUC()
         {
@@ -14,8 +19,40 @@
             WeakEventManager<UserControl, RoutedEventArgs>.AddHandler(this, "Loaded", this.OnLoaded);
         }
 
+        public XamlProperty<List<string>> FilterdComboBoxSource { get; set; } = XamlProperty.Set<List<string>>();
+
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
+            List<CheckComboBoxTest> data = new List<CheckComboBoxTest>();
+            data.Add(new CheckComboBoxTest(1, "C#"));
+            data.Add(new CheckComboBoxTest(2, "C++"));
+            data.Add(new CheckComboBoxTest(3, "VB.Net"));
+            data.Add(new CheckComboBoxTest(4, "Javascript"));
+            data.Add(new CheckComboBoxTest(5, "Object C"));
+            data.Add(new CheckComboBoxTest(6, "Java"));
+
+            this.FilterdComboBoxSource.Value = new List<string> { "Affe", "Bär", "Ameise", "Igel", "Elefant", "Hund", "Pferd", "Pinguin", "Zebra", "2001", "2010", "2024", "2030" };
+
+            this.DataContext = this;
         }
+
+        #region PropertyChanged Implementierung
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion PropertyChanged Implementierung
     }
 }
---
+++ ModernUILibrary/ModernUI/ComboBox/MComboBox.cs
@@ -0,0 +1,35 @@
+﻿namespace ModernIU.Controls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class MComboBox : ComboBox
+    {
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register("CornerRadius" , typeof(System.Windows.CornerRadius), typeof(MComboBox));
+
+        public System.Windows.CornerRadius CornerRadius
+        {
+            get { return (System.Windows.CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty WatermarkProperty = DependencyProperty.Register("Watermark" , typeof(string), typeof(MComboBox));
+
+        public string Watermark
+        {
+            get { return (string)GetValue(WatermarkProperty); }
+            set { SetValue(WatermarkProperty, value); }
+        }
+
+        static MComboBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MComboBox), new FrameworkPropertyMetadata(typeof(MComboBox)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+        }
+    }
+}
