e160219783adbfb504364df7a1bb6f706e874401
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2024-11-15 20:28:18 +0100

    *

---
+++ ModernUILibrary/ModernUI/ContentNavigation/ContentNavigation.cs
@@ -0,0 +1,18 @@
+﻿namespace ModernIU.Controls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class ContentNavigation : ContentControl
+    {
+        static ContentNavigation()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ContentNavigation), new FrameworkPropertyMetadata(typeof(ContentNavigation)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+    }
+}
