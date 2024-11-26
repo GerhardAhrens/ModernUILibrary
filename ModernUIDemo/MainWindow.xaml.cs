539ad7c84b8d83e06a5287636355a1f1279509f6
Gerhard Ahrens <gerhard.ahrens@pta.de>
2024-11-26 08:11:46 +0100

    *

--- ModernUIDemo/MainWindow.xaml.cs
+++ ModernUIDemo/MainWindow.xaml.cs
@@ -33,73 +33,75 @@
 
             this.tabItemSource.Add(new TabControlItem("Eingabe", true));
             this.tabItemSource.Add(new TabControlItem("TextBox (String) Controls", new TextBoxStringControlsUC()) { Stichworte = "TextBox;String;Text;Icon" });
-            this.tabItemSource.Add(new TabControlItem("TextBox (Numeric) Controls", new TextBoxNumericControlsUC()));
-            this.tabItemSource.Add(new TabControlItem("TextBox Multiline Controls", new TextBoxMultilineControlsUC()));
-            this.tabItemSource.Add(new TabControlItem("TextBox RTF Controls", new TextBoxRtfControlsUC()));
-            this.tabItemSource.Add(new TabControlItem("TextBox RTF HTML Controls", new TextBoxRtfHTMLControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("TextBox (Numeric) Controls", new TextBoxNumericControlsUC()) { Stichworte = "TextBox;Zahl;Numeric" });
+            this.tabItemSource.Add(new TabControlItem("NumericUpDown Controls", new NumericUpDownControlsUC()) { Stichworte = "TextBox;Zahl;Numeric" });
+            this.tabItemSource.Add(new TabControlItem("TextBox Multiline Controls", new TextBoxMultilineControlsUC()) { Stichworte = "TextBox;String;Text" });
+            this.tabItemSource.Add(new TabControlItem("TextBox RTF Controls", new TextBoxRtfControlsUC()) { Stichworte = "TextBox;String;Text;rtf" });
             this.tabItemSource.Add(new TabControlItem("CheckBox Controls", new CheckBoxUC()){ Stichworte = "CheckBox;Flat"} );
             this.tabItemSource.Add(new TabControlItem("DateTime Picker Controls", new DateTimeControlsUC()) { Stichworte = "DateTime;Datum;Flat;Time;Picker" });
 
-            tabItemSource.Add(new TabControlItem("Button", true));
-            tabItemSource.Add(new TabControlItem("Button Controls", new ButtonControlsUC()));
-            tabItemSource.Add(new TabControlItem("DropDownButton Controls", new DropDownButtonControlsUC()));
-            tabItemSource.Add(new TabControlItem("RadioButton Controls", new RadioButtonControlsUC()));
-            tabItemSource.Add(new TabControlItem("NumericUpDown Controls", new NumericUpDownControlsUC()));
-            tabItemSource.Add(new TabControlItem("FloatingAction Controls", new FloatingActionControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Button", true));
+            this.tabItemSource.Add(new TabControlItem("Button Controls", new ButtonControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("DropDownButton Controls", new DropDownButtonControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("RadioButton Controls", new RadioButtonControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("FloatingAction Controls", new FloatingActionControlsUC()));
 
-            tabItemSource.Add(new TabControlItem("Ausgabe/Anzeige", true));
-            tabItemSource.Add(new TabControlItem("TextBlock Controls", new TextBlockControlsUC()) { Stichworte = "TextBlock;Anzeigen;Animation;Search" });
-            tabItemSource.Add(new TabControlItem("ListBox/ComboBox Controls", new ListBoxControlsUC()) { Stichworte = "ListBox;ComboBox;Flat" });
-            tabItemSource.Add(new TabControlItem("ListTextBox Controls", new ListTextBoxControlsUC()) { Stichworte = "ListBox;Flat;ListView" });
-            tabItemSource.Add(new TabControlItem("ComboTree Controls", new ComboTreeControlsUC()) { Stichworte = "ComboBox;Tree;Flat" });
-            tabItemSource.Add(new TabControlItem("ComboBox Controls", new ComboBoxControlsUC()) { Stichworte = "ComboBox;Flat" });
-            tabItemSource.Add(new TabControlItem("CascaderBox Controls", new CascaderBoxControlsUC()) { Stichworte = "ComboBox;Flat;Tree" });
-            tabItemSource.Add(new TabControlItem("LED Controls", new LedControlsUC()) { Stichworte = "Anzeige; LED" });
-            tabItemSource.Add(new TabControlItem("Dashboard Controls", new DashboardControlsUC()) {Stichworte="Anzeige" });
-            tabItemSource.Add(new TabControlItem("Accordion Controls", new AccordionControlsUC()) { Stichworte = "Anzeige;Accordion" });
+            this.tabItemSource.Add(new TabControlItem("Ausgabe/Anzeige", true));
+            this.tabItemSource.Add(new TabControlItem("TextBlock Controls", new TextBlockControlsUC()) { Stichworte = "TextBlock;Anzeigen;Animation;Search" });
+            this.tabItemSource.Add(new TabControlItem("TextBox RTF HTML Controls", new TextBoxRtfHTMLControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("ListBox/ComboBox Controls", new ListBoxControlsUC()) { Stichworte = "ListBox;ComboBox;Flat" });
+            this.tabItemSource.Add(new TabControlItem("ListTextBox Controls", new ListTextBoxControlsUC()) { Stichworte = "ListBox;Flat;ListView" });
+            this.tabItemSource.Add(new TabControlItem("ComboTree Controls", new ComboTreeControlsUC()) { Stichworte = "ComboBox;Tree;Flat" });
+            this.tabItemSource.Add(new TabControlItem("ComboBox Controls", new ComboBoxControlsUC()) { Stichworte = "ComboBox;Flat" });
+            this.tabItemSource.Add(new TabControlItem("CascaderBox Controls", new CascaderBoxControlsUC()) { Stichworte = "ComboBox;Flat;Tree" });
+            this.tabItemSource.Add(new TabControlItem("LED Controls", new LedControlsUC()) { Stichworte = "Anzeige; LED" });
+            this.tabItemSource.Add(new TabControlItem("Dashboard Controls", new DashboardControlsUC()) {Stichworte="Anzeige" });
+            this.tabItemSource.Add(new TabControlItem("Accordion Controls", new AccordionControlsUC()) { Stichworte = "Anzeige;Accordion" });
 
-            tabItemSource.Add(new TabControlItem("Layout Grid, Panel, Separator", true));
-            tabItemSource.Add(new TabControlItem("LayoutPanel Controls", new LayoutPanelControlsUC()));
-            tabItemSource.Add(new TabControlItem("Separator Controls", new SeparatorControlsUC()));
-            tabItemSource.Add(new TabControlItem("Grid ContentFrame", new ContentFrameControlsUC()));
-            tabItemSource.Add(new TabControlItem("TabControl", new TabControlControlsUC()) { Stichworte = "Anzeige;TabControl;Register;Layout" });
-            tabItemSource.Add(new TabControlItem("CarouselControl", new CarouselControlsUC()) { Stichworte = "Anzeige;SlideSwitch;Carousel;Karusell;Layout" });
-            tabItemSource.Add(new TabControlItem("TagControl", new TagControlsUC()) { Stichworte = "Anzeige;Tag;Layout" });
+            this.tabItemSource.Add(new TabControlItem("Layout Grid, Panel, Separator", true));
+            this.tabItemSource.Add(new TabControlItem("LayoutPanel Controls", new LayoutPanelControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Separator Controls", new SeparatorControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Grid ContentFrame", new ContentFrameControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("TabControl", new TabControlControlsUC()) { Stichworte = "Anzeige;TabControl;Register;Layout" });
+            this.tabItemSource.Add(new TabControlItem("CarouselControl", new CarouselControlsUC()) { Stichworte = "Anzeige;SlideSwitch;Carousel;Karusell;Layout" });
+            this.tabItemSource.Add(new TabControlItem("TagControl", new TagControlsUC()) { Stichworte = "Anzeige;Tag;Layout" });
 
-            tabItemSource.Add(new TabControlItem("Listen, Collection Darstellung", true));
-            tabItemSource.Add(new TabControlItem("FlatListView Control", new FlatListViewControlsUC()) { Stichworte = "ListView;Flat" });
+            this.tabItemSource.Add(new TabControlItem("Listen, Collection Darstellung", true));
+            this.tabItemSource.Add(new TabControlItem("FlatListView Control", new FlatListViewControlsUC()) { Stichworte = "ListView;Flat" });
 
-            tabItemSource.Add(new TabControlItem("View, Loading", true));
-            tabItemSource.Add(new TabControlItem("Badges Controls", new BadgesControlsUC()));
-            tabItemSource.Add(new TabControlItem("BusyIndicator Controls", new BusyIndicatorControlsUC()));
-            tabItemSource.Add(new TabControlItem("Loading Animation Controls", new LoadingControlsUC()));
-            tabItemSource.Add(new TabControlItem("Slider Controls", new SliderControlsUC()) { Stichworte = "Slider;Flat" });
-            tabItemSource.Add(new TabControlItem("ProgressBar Controls", new ProgressBarControlsUC()));
-            tabItemSource.Add(new TabControlItem("Tooltip Controls", new TooltipControlsUC()));
-            tabItemSource.Add(new TabControlItem("GroupBox Controls", new GroupBoxControlsUC()));
-            tabItemSource.Add(new TabControlItem("Rating Controls", new RatingControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("View, Loading", true));
+            this.tabItemSource.Add(new TabControlItem("Badges Controls", new BadgesControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("BusyIndicator Controls", new BusyIndicatorControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Loading Animation Controls", new LoadingControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Slider Controls", new SliderControlsUC()) { Stichworte = "Slider;Flat" });
+            this.tabItemSource.Add(new TabControlItem("ProgressBar Controls", new ProgressBarControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Tooltip Controls", new TooltipControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("GroupBox Controls", new GroupBoxControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Rating Controls", new RatingControlsUC()));
 
-            tabItemSource.Add(new TabControlItem($"MessageBox, NoticeMessage\nWindow", true));
-            tabItemSource.Add(new TabControlItem("NoticeMessage Controls", new NoticeMessageControlsUC()));
-            tabItemSource.Add(new TabControlItem("MessageBox Window", new MessageBoxControlsUC()));
-            tabItemSource.Add(new TabControlItem("NotificationBox Window", new NotifiactionBoxControlsUC()));
-            tabItemSource.Add(new TabControlItem("Notification Service Dialog", new NotificationServiceControlsUC()));
-            tabItemSource.Add(new TabControlItem("Messaging mit zwei Controls", new MessagingAControlsUC()));
-            tabItemSource.Add(new TabControlItem("Messaging B", new MessagingBControlsUC()));
-            tabItemSource.Add(new TabControlItem("Flyout Window", new FlyoutControlsUC()));
-            tabItemSource.Add(new TabControlItem("Window", new WindowControlsUC()));
-            tabItemSource.Add(new TabControlItem("PopUp Window", new PopUpControlsUC()));
+            this.tabItemSource.Add(new TabControlItem($"MessageBox, NoticeMessage\nWindow", true));
+            this.tabItemSource.Add(new TabControlItem("NoticeMessage Controls", new NoticeMessageControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("MessageBox Window", new MessageBoxControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("NotificationBox Window", new NotifiactionBoxControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Notification Service Dialog", new NotificationServiceControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Messaging mit zwei Controls", new MessagingAControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Messaging B", new MessagingBControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Flyout Window", new FlyoutControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Window", new WindowControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("PopUp Window", new PopUpControlsUC()));
 
-            tabItemSource.Add(new TabControlItem($"Dateien, IO", true));
-            tabItemSource.Add(new TabControlItem("Datei/Verzeichnis Controls", new ChooseBoxControlsUC()));
-            tabItemSource.Add(new TabControlItem("Upload Controls", new UploadControlsUC()));
+            this.tabItemSource.Add(new TabControlItem($"Dateien, IO", true));
+            this.tabItemSource.Add(new TabControlItem("Datei/Verzeichnis Controls", new ChooseBoxControlsUC()));
+            this.tabItemSource.Add(new TabControlItem("Upload Controls", new UploadControlsUC()));
 
-            tabItemSource.Add(new TabControlItem($"Behavior Control\nErweiterungen", true));
-            tabItemSource.Add(new TabControlItem("TextBlock Controls", new BehaviorsControlsUC()) { Stichworte="TextBlock;" });
-            tabItemSource.Add(new TabControlItem("TextBox Controls", new BehaviorTxTControlsUC()) { Stichworte="TextBox;Eingabe;Input;Masken;Pattern"});
-            tabItemSource.Add(new TabControlItem("TextBox Watermarket", new BehaviorWaterMControlsUC()) { Stichworte = "TextBox;Wasserzeichen;Watermarket;Behavior" });
-            tabItemSource.Add(new TabControlItem("Excel Cell Behavior für Controls", new BehaviorExcelCellControlsUC()) { Stichworte = "Excel;Cell;Behavior" });
-            tabItemSource.Add(new TabControlItem("CheckBox Behavior", new BehaviorCheckBoxUC()) {Stichworte = "CheckBox;Behavior" });
+            this.tabItemSource.Add(new TabControlItem($"Behavior Control\nErweiterungen", true));
+            this.tabItemSource.Add(new TabControlItem("TextBlock Controls", new BehaviorsControlsUC()) { Stichworte="TextBlock;" });
+            this.tabItemSource.Add(new TabControlItem("TextBox Controls", new BehaviorTxTControlsUC()) { Stichworte="TextBox;Eingabe;Input;Masken;Pattern"});
+            this.tabItemSource.Add(new TabControlItem("TextBox Watermarket", new BehaviorWaterMControlsUC()) { Stichworte = "TextBox;Wasserzeichen;Watermarket;Behavior" });
+            this.tabItemSource.Add(new TabControlItem("Excel Cell Behavior für Controls", new BehaviorExcelCellControlsUC()) { Stichworte = "Excel;Cell;Behavior" });
+            this.tabItemSource.Add(new TabControlItem("CheckBox Behavior", new BehaviorCheckBoxUC()) {Stichworte = "CheckBox;Behavior" });
+
+            this.CountSamples = this.tabItemSource.Count(c => c.IsGroupItem == false);
 
             this.DataContext = this;
         }
@@ -143,6 +145,18 @@
                 this.filterText = value;
                 this.OnPropertyChanged();
                 this.RefreshDefaultFilter(value);
+            }
+        }
+
+        private int countSamples;
+
+        public int CountSamples
+        {
+            get { return countSamples; }
+            set
+            {
+                this.countSamples = value;
+                this.OnPropertyChanged();
             }
         }
 
@@ -191,6 +205,8 @@
                     return true;
                 }
             };
+
+            this.ListBoxSource.MoveCurrentTo(2);
         }
 
         private void OnWindowClosed(object sender, EventArgs e)
