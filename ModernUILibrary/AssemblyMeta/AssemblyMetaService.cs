43cc69bc806e21fffc8daa399d4d76ee8df5e0a6
Gerhard Ahrens <gerhard.ahrens@pta.de>
2024-12-04 16:01:57 +0100

    *

---
+++ ModernUILibrary/AssemblyMeta/AssemblyMetaService.cs
@@ -0,0 +1,43 @@
+//-----------------------------------------------------------------------
+// <copyright file="AssemblyMetaService.cs" company="www.pta.de">
+//     Class: AssemblyMetaService
+//     Copyright © www.pta.de 2024
+// </copyright>
+//
+// <author>Gerhard Ahrens - www.pta.de</author>
+// <email>gerhard.ahrens@pta.de</email>
+// <date>04.12.2024 15:59:19</date>
+//
+// <summary>
+// Klasse für 
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace ModernUILibrary.AssemblyMeta
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ModernIU.Base;
+
+    public class AssemblyMetaService : DisposableCoreBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssemblyMetaService"/> class.
+        /// </summary>
+        public AssemblyMetaService()
+        {
+        }
+
+        protected override void DisposeManagedResources()
+        {
+        }
+
+        protected override void DisposeUnmanagedResources()
+        {
+        }
+    }
+}
