efe0f4c0c5b34e434a2e8293e7fb19007aef42e9
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-01-21 21:47:18 +0100

    Dbase Provider und Test

---
+++ ModernTest/ModernBaseLibrary/Data/DBase/DBaseWrite_Test.cs
@@ -0,0 +1,113 @@
+﻿namespace ModernTest.ModernBaseLibrary.Core
+{
+    using System.Diagnostics;
+    using System;
+    using System.IO;
+
+    using global::ModernBaseLibrary.Core.IO;
+    using global::ModernBaseLibrary.Cryptography;
+    using global::ModernBaseLibrary.LinqToFile;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using System.Linq;
+    using global::ModernBaseLibrary.Extension;
+    using System.Globalization;
+    using System.Threading;
+    using global::ModernBaseLibrary.ExcelReader;
+    using System.Data;
+    using global::ModernBaseLibrary.DBase;
+
+    [TestClass]
+    public class DBaseWrite_Test : BaseTest
+    {
+        public string TestDirPath => TestContext.TestRunDirectory;
+
+        public string TempDirPath => Path.Combine(TestDirPath, "Temp");
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            CultureInfo culture = new CultureInfo("de-DE");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+            Directory.CreateDirectory(TempDirPath);
+        }
+
+        [TestCleanup]
+        public void Clean()
+        {
+            if (Directory.Exists(TempDirPath))
+            {
+                Directory.Delete(TempDirPath, true);
+            }
+        }
+
+        [TestMethod]
+        public void WriteNoData()
+        {
+            DirectoryInfo di = new DirectoryInfo(TempDirPath);
+            string pathFileName = Path.GetFullPath($"{di.Parent.Parent.Parent}\\ModernTest\\ModernBaseLibrary\\Data\\DBase\\DemoData\\dbaseWrite.dbf");
+
+            using (Dbf dbf = new Dbf())
+            {
+                dbf.Write(pathFileName, DbfVersion.VisualFoxPro);
+                byte[] data = ReadBytes(pathFileName);
+                Assert.AreEqual(0x30, data[0], "Version should be 0x30.");
+            }
+        }
+
+        [TestMethod]
+        public void WriteOneField()
+        {
+            DirectoryInfo di = new DirectoryInfo(TempDirPath);
+            string pathFileName = Path.GetFullPath($"{di.Parent.Parent.Parent}\\ModernTest\\ModernBaseLibrary\\Data\\DBase\\DemoData\\dbaseReadWriteA.dbf");
+
+            using (Dbf dbf = new Dbf())
+            {
+                DbfField field = new DbfField("TEST", DbfFieldType.Character, 12);
+                dbf.Fields.Add(field);
+                dbf.Write(pathFileName, DbfVersion.VisualFoxPro);
+
+                Dbf dbfRead = new Dbf();
+                dbfRead.Read(pathFileName);
+
+                Assert.AreEqual("TEST", dbfRead.Fields[0].Name, "Field name should be TEST.");
+            }
+        }
+
+        [TestMethod]
+        public void WriteFieldAndRecord()
+        {
+            DirectoryInfo di = new DirectoryInfo(TempDirPath);
+            string pathFileName = Path.GetFullPath($"{di.Parent.Parent.Parent}\\ModernTest\\ModernBaseLibrary\\Data\\DBase\\DemoData\\dbaseReadWriteB.dbf");
+
+            using (Dbf dbf = new Dbf())
+            {
+                DbfField field = new DbfField("TEST", DbfFieldType.Character, 12);
+                dbf.Fields.Add(field);
+                DbfRecord record = dbf.CreateRecord();
+                record.Data[0] = "HELLO";
+                dbf.Write(pathFileName, DbfVersion.VisualFoxPro);
+
+                Dbf dbfRead = new Dbf();
+                dbfRead.Read(pathFileName);
+
+                Assert.AreEqual("HELLO", dbf.Records[0][0], "Record content should be HELLO.");
+            }
+        }
+
+
+        private byte[] ReadBytes(string fileName)
+        {
+            // Open stream for reading.
+            FileStream stream = File.Open(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(stream);
+            byte[] data = new byte[stream.Length];
+            data = reader.ReadBytes((int)stream.Length);
+            reader.Close();
+            stream.Close();
+            return data;
+        }
+    }
+}
---
+++ ModernTest/ModernBaseLibrary/Data/DBase/DBase_Test.cs
@@ -0,0 +1,102 @@
+﻿namespace ModernTest.ModernBaseLibrary.Core
+{
+    using System.Diagnostics;
+    using System;
+    using System.IO;
+
+    using global::ModernBaseLibrary.Core.IO;
+    using global::ModernBaseLibrary.Cryptography;
+    using global::ModernBaseLibrary.LinqToFile;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using System.Linq;
+    using global::ModernBaseLibrary.Extension;
+    using System.Globalization;
+    using System.Threading;
+    using global::ModernBaseLibrary.ExcelReader;
+    using System.Data;
+    using global::ModernBaseLibrary.DBase;
+
+    [TestClass]
+    public class DBase_Test : BaseTest
+    {
+        public string TestDirPath => TestContext.TestRunDirectory;
+
+        public string TempDirPath => Path.Combine(TestDirPath, "Temp");
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            CultureInfo culture = new CultureInfo("de-DE");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+            Directory.CreateDirectory(TempDirPath);
+        }
+
+        [TestCleanup]
+        public void Clean()
+        {
+            if (Directory.Exists(TempDirPath))
+            {
+                Directory.Delete(TempDirPath, true);
+            }
+        }
+
+        [TestMethod]
+        public void DBase3WithMemo()
+        {
+            DirectoryInfo di = new DirectoryInfo(TempDirPath);
+            string pathFileName = Path.GetFullPath($"{di.Parent.Parent.Parent}\\ModernTest\\ModernBaseLibrary\\Data\\DBase\\DemoData\\dbase_83.dbf");
+            if (File.Exists(pathFileName) == true)
+            {
+                using (Dbf dbf = new Dbf())
+                {
+                    dbf.Read(pathFileName);
+                    Assert.AreEqual(67, dbf.Records.Count, "Read 67 Datensätze");
+                    Assert.AreEqual(15, dbf.Fields.Count, "Read 15 Columns");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Dbase3WithMemoStreamRead()
+        {
+            DirectoryInfo di = new DirectoryInfo(TempDirPath);
+            string pathFileNameDBF = Path.GetFullPath($"{di.Parent.Parent.Parent}\\ModernTest\\ModernBaseLibrary\\Data\\DBase\\DemoData\\dbase_83.dbf");
+            string pathFileNameDBT = Path.GetFullPath($"{di.Parent.Parent.Parent}\\ModernTest\\ModernBaseLibrary\\Data\\DBase\\DemoData\\dbase_83.dbt");
+            if (File.Exists(pathFileNameDBF) == true)
+            {
+                using (Dbf dbf = new Dbf())
+                {
+                    using (FileStream baseStream = File.Open(pathFileNameDBF, FileMode.Open, FileAccess.Read))
+                    using (FileStream memoStream = File.Open(pathFileNameDBT, FileMode.Open, FileAccess.Read))
+
+                    dbf.Read(baseStream, memoStream);
+
+                    Assert.AreEqual(67, dbf.Records.Count, "Read 67 Datensätze");
+                    Assert.AreEqual(15, dbf.Fields.Count, "Read 15 Columns");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void DBase3WithoutMemo()
+        {
+            DirectoryInfo di = new DirectoryInfo(TempDirPath);
+            string pathFileName = Path.GetFullPath($"{di.Parent.Parent.Parent}\\ModernTest\\ModernBaseLibrary\\Data\\DBase\\DemoData\\dbase_03.dbf");
+            if (File.Exists(pathFileName) == true)
+            {
+                using (Dbf dbf = new Dbf())
+                {
+                    dbf.Read(pathFileName);
+                    Assert.AreEqual(14, dbf.Records.Count, "Read 14 Datensätze");
+                    Assert.AreEqual(31, dbf.Fields.Count, "Read 31 Columns");
+                    Assert.AreEqual("Point_ID", dbf.Fields[0].Name, "First field name should be 'Point_ID'");
+                    Assert.AreEqual(12, dbf.Fields[0].Length, "Point_ID field length must be 12.");
+                    Assert.AreEqual(3, dbf.Fields[23].Precision, "GPS_Second field length must be 3.");
+                }
+            }
+        }
+    }
+}
