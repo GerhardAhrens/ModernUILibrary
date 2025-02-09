60cc52cf2c458196425a302cedf5bbb457090fad
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-02-09 19:55:23 +0100

    *

--- ModernUILibrary/ModernUI/TabControl/LeftTabControl.cs
+++ ModernUILibrary/ModernUI/TabControl/LeftTabControl.cs
@@ -7,31 +7,18 @@
 
     public class LeftTabControl : TabControl
     {
-        public static readonly DependencyProperty HeaderContentProperty = DependencyProperty.Register("HeaderContent", typeof(object), typeof(LeftTabControl));
-
         static LeftTabControl()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(LeftTabControl), new FrameworkPropertyMetadata(typeof(LeftTabControl)));
+            //DefaultStyleKeyProperty.OverrideMetadata(typeof(LeftTabControl), new FrameworkPropertyMetadata(typeof(LeftTabControl)));
         }
 
         public LeftTabControl()
         {
-            this.FontSize = ControlBase.FontSize;
-            this.FontFamily = ControlBase.FontFamily;
-            this.Focusable = true;
-            this.TabStripPlacement = Dock.Left;
-            this.SetValue(Grid.IsSharedSizeScopeProperty, true);
-        }
-
-        public object HeaderContent
-        {
-            get { return (object)GetValue(HeaderContentProperty); }
-            set { SetValue(HeaderContentProperty, value); }
-        }
-
-        protected override DependencyObject GetContainerForItemOverride()
-        {
-            return new TabItem();
+            //this.FontSize = ControlBase.FontSize;
+            //this.FontFamily = ControlBase.FontFamily;
+            //this.Focusable = true;
+            //this.TabStripPlacement = Dock.Top;
+            //this.SetValue(Grid.IsSharedSizeScopeProperty, true);
         }
     }
 }
