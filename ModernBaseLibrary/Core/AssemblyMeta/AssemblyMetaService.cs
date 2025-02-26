11eb1c4e13751c66eed5d67c6a85ae40205f64ec
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-02-26 20:48:41 +0100

    *

--- ModernBaseLibrary/Core/AssemblyMeta/AssemblyMetaService.cs
+++ ModernBaseLibrary/Core/AssemblyMeta/AssemblyMetaService.cs
@@ -16,12 +16,8 @@
 namespace ModernBaseLibrary.Core
 {
     using System;
-    using System.Collections;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Reflection;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class AssemblyMetaService : DisposableCoreBase
     {
