0af7708c947043f518fef877cd4e49b317611d2a
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-03-01 20:27:46 +0100

    *

--- ModernUILibrary/ModernUI/Label/LinkLabel.cs
+++ ModernUILibrary/ModernUI/Label/LinkLabel.cs
@@ -1,40 +1,21 @@
-//-----------------------------------------------------------------------
-// <copyright file="LinkLabel.cs" company="Lifeprojects.de">
-//     Class: LinkLabel
-//     Copyright © Gerhard Ahrens, 2023
-// </copyright>
-//
-// <author>Gerhard Ahrens - Lifeprojects.de</author>
-// <email>development@lifeprojects.de</email>
-// <date>29.04.2023</date>
-//
-// <summary>Class for UI Control LinkLabel</summary>
-// <website>
-// https://www.cnblogs.com/lazycoding/archive/2012/10/11/2719778.html
-// </website>
-//-----------------------------------------------------------------------
-
-namespace ModernIU.Controls
+ï»¿namespace ModernIU.Controls
 {
     using System;
-    using System.ComponentModel;
-    using System.Windows;
+    using System.Collections.Generic;
+    using System.Text;
     using System.Windows.Controls;
+    using System.Windows;
+    using System.ComponentModel;
     using System.Windows.Media;
+    using System.Windows.Input;
+    using System.Windows.Navigation;
+    using System.Windows.Documents;
 
+    [TemplatePart(Name = "PART_InnerHyperlink", Type = typeof(Hyperlink))]
     public class LinkLabel : Label
     {
-        private const string _linkLabel = "LinkLabel";
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="LinkLabel"/> class.
-        /// </summary>
-        static LinkLabel()
-        {
-            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(LinkLabel), new FrameworkPropertyMetadata(typeof(LinkLabel)));
-        }
-
-        public static readonly DependencyProperty UrlProperty = DependencyProperty.Register("Url", typeof(Uri), typeof(LinkLabel));
+        public static readonly DependencyProperty UrlProperty =
+            DependencyProperty.Register("Url", typeof(Uri), typeof(LinkLabel));
 
         [Category("Common Properties"), Bindable(true)]
         public Uri Url
@@ -43,7 +24,9 @@ namespace ModernIU.Controls
             set { SetValue(UrlProperty, value); }
         }
 
-        public static readonly DependencyProperty HyperlinkStyleProperty = DependencyProperty.Register("HyperlinkStyle", typeof(Style), typeof(LinkLabel));
+        public static readonly DependencyProperty HyperlinkStyleProperty =
+            DependencyProperty.Register("HyperlinkStyle", typeof(Style),
+                typeof(LinkLabel));
 
         public Style HyperlinkStyle
         {
@@ -51,13 +34,123 @@ namespace ModernIU.Controls
             set { SetValue(HyperlinkStyleProperty, value); }
         }
 
-        public static readonly DependencyProperty HoverForegroundProperty = DependencyProperty.Register("HoverForeground", typeof(Brush), typeof(LinkLabel));
+        public static readonly DependencyProperty HoverForegroundProperty =
+            DependencyProperty.Register("HoverForeground", typeof(Brush),
+                typeof(LinkLabel));
 
         [Category("Brushes"), Bindable(true)]
         public Brush HoverForeground
         {
             get { return GetValue(HoverForegroundProperty) as Brush; }
             set { SetValue(HoverForegroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty LinkLabelBehaviorProperty =
+            DependencyProperty.Register("LinkLabelBehavior",
+                typeof(LinkLabelBehavior),
+                typeof(LinkLabel));
+
+        [Category("Common Properties"), Bindable(true)]
+        public LinkLabelBehavior LinkLabelBehavior
+        {
+            get { return (LinkLabelBehavior)GetValue(LinkLabelBehaviorProperty); }
+            set { SetValue(LinkLabelBehaviorProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(object), typeof(LinkLabel));
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(LinkLabel));
+
+        public static readonly DependencyProperty CommandTargetProperty =
+            DependencyProperty.Register("CommandTarget", typeof(IInputElement), typeof(LinkLabel));
+
+        [Localizability(LocalizationCategory.NeverLocalize), Bindable(true), Category("Action")]
+        public object CommandParameter
+        {
+            get { return this.GetValue(CommandParameterProperty); }
+            set { this.SetValue(CommandParameterProperty, value); }
+        }
+
+        [Localizability(LocalizationCategory.NeverLocalize), Bindable(true), Category("Action")]
+        public ICommand Command
+        {
+            get { return (ICommand)this.GetValue(CommandParameterProperty); }
+            set { this.SetValue(CommandParameterProperty, value); }
+        }
+
+        [Bindable(true), Category("Action")]
+        public IInputElement CommandTarget
+        {
+            get { return (IInputElement)this.GetValue(CommandTargetProperty); }
+            set { this.SetValue(CommandTargetProperty, value); }
+        }
+
+        [Category("Behavior")]
+        public static readonly RoutedEvent ClickEvent;
+
+        [Category("Behavior")]
+        public static readonly RoutedEvent RequestNavigateEvent;
+
+        static LinkLabel()
+        {
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(LinkLabel),
+                new FrameworkPropertyMetadata(typeof(LinkLabel)));
+
+            ClickEvent = EventManager.RegisterRoutedEvent("Click", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(LinkLabel));
+            RequestNavigateEvent = EventManager.RegisterRoutedEvent("RequestNavigate", RoutingStrategy.Bubble, typeof(RequestNavigateEventHandler), typeof(LinkLabel));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            Hyperlink innerHyperlink = GetTemplateChild("PART_InnerHyperlink") as Hyperlink;
+            if (innerHyperlink != null)
+            {
+                innerHyperlink.Click += new RoutedEventHandler(InnerHyperlink_Click);
+                innerHyperlink.RequestNavigate += new RequestNavigateEventHandler(InnerHyperlink_RequestNavigate);
+            }
+        }
+
+        void InnerHyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            RequestNavigateEventArgs args = new RequestNavigateEventArgs(e.Uri, String.Empty);
+            args.Source = this;
+            args.RoutedEvent = LinkLabel.RequestNavigateEvent;
+            RaiseEvent(args);
+        }
+
+        
+        void InnerHyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(LinkLabel.ClickEvent, this));
+        }
+
+        public event RoutedEventHandler Click
+        {
+            add
+            {
+                base.AddHandler(ClickEvent, value);
+            }
+            remove
+            {
+                base.RemoveHandler(ClickEvent, value);
+            }
+        }
+
+        public event RequestNavigateEventHandler RequestNavigate
+        {
+            add
+            {
+                base.AddHandler(RequestNavigateEvent, value);
+            }
+            remove
+            {
+                base.RemoveHandler(RequestNavigateEvent, value);
+            }
         }
     }
 }
---
+++ ModernUILibrary/ModernUI/Label/LinkLabelBehavior.cs
@@ -0,0 +1,14 @@
+﻿namespace ModernIU.Controls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public enum LinkLabelBehavior
+    {
+        SystemDefault,
+        AlwaysUnderline,
+        HoverUnderline,
+        NeverUnderline
+    }
+}
