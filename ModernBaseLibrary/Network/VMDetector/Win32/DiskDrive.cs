389503a6dbddd7d589e3ed1e893d1b4fa4ff3d70
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-03-11 19:21:35 +0100

    *

---
+++ ModernBaseLibrary/Network/VMDetector/Win32/DiskDrive.cs
@@ -0,0 +1,13 @@
+﻿namespace ModernBaseLibrary.Network
+{
+    using System.Management;
+
+    public class DiskDrive : BaseWin32Entity
+    {
+        // https://msdn.microsoft.com/en-us/library/aa394132(v=vs.85).aspx
+
+        public DiskDrive(ManagementBaseObject obj) : base(obj)
+        {
+        }
+    }
+}
