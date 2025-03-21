e2d49125c0d16bc4ecd4968ccbc6d433eb950361
Gerhard Ahrens <gerhard.ahrens@pta.de>
2025-03-21 14:43:39 +0100

    Fuktion für Zoomdarstellung mit  dem Event PreviewMouseWheel

--- ModernUIDemo/MyControls/ColorControlsUC.xaml.cs
+++ ModernUIDemo/MyControls/ColorControlsUC.xaml.cs
@@ -2,6 +2,7 @@
 {
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Input;
 
     /// <summary>
     /// Interaktionslogik für ColorControlsUC.xaml
@@ -12,11 +13,36 @@
         {
             this.InitializeComponent();
             WeakEventManager<UserControl, RoutedEventArgs>.AddHandler(this, "Loaded", this.OnLoaded);
+            WeakEventManager<UserControl, MouseWheelEventArgs>.AddHandler(this, "PreviewMouseWheel", this.OnPreviewMouseWheel);
         }
 
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) == true)
+            {
+                if (e.Delta > 0)
+                {
+                    if (this.Scalefactor.ScaleX <= 2.0)
+                    {
+                        this.Scalefactor.ScaleX = this.Scalefactor.ScaleX + 0.20;
+                        this.Scalefactor.ScaleY = this.Scalefactor.ScaleY + 0.20;
+                    }
+                }
+
+                if (e.Delta < 0)
+                {
+                    if (this.Scalefactor.ScaleX > 1.00)
+                    {
+                        this.Scalefactor.ScaleX = this.Scalefactor.ScaleX - 0.20;
+                        this.Scalefactor.ScaleY = this.Scalefactor.ScaleY - 0.20;
+                    }
+                }
+            }
         }
     }
 }
--- ModernUIDemo/MyControls/IconsControlsUC.xaml.cs
+++ ModernUIDemo/MyControls/IconsControlsUC.xaml.cs
@@ -1,6 +1,8 @@
 ﻿namespace ModernUIDemo.MyControls
 {
+    using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Input;
 
     /// <summary>
     /// Interaktionslogik für IconsControlsUC.xaml
@@ -10,6 +12,31 @@
         public IconsControlsUC()
         {
             this.InitializeComponent();
+            WeakEventManager<UserControl, MouseWheelEventArgs>.AddHandler(this, "PreviewMouseWheel", this.OnPreviewMouseWheel);
+        }
+
+        private void OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) == true)
+            {
+                if (e.Delta > 0)
+                {
+                    if (this.Scalefactor.ScaleX <= 2.0)
+                    {
+                        this.Scalefactor.ScaleX = this.Scalefactor.ScaleX + 0.20;
+                        this.Scalefactor.ScaleY = this.Scalefactor.ScaleY + 0.20;
+                    }
+                }
+
+                if (e.Delta < 0)
+                {
+                    if (this.Scalefactor.ScaleX > 1.00)
+                    {
+                        this.Scalefactor.ScaleX = this.Scalefactor.ScaleX - 0.20;
+                        this.Scalefactor.ScaleY = this.Scalefactor.ScaleY - 0.20;
+                    }
+                }
+            }
         }
     }
 }
