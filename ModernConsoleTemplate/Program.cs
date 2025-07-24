9d0d86c768f54060c8d8b89bba5cbf12c33c7128
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-07-24 06:48:15 +0200

    Update Console Template

---
+++ ModernConsoleTemplate/Program.cs
@@ -0,0 +1,64 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Lifeprojects.de">
+//     Class: Program
+//     Copyright © Lifeprojects.de 2025
+// </copyright>
+// <Template>
+// 	Version 2.0.2025.0, 28.4.2025
+// </Template>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>04.05.2025 19:34:00</date>
+//
+// <summary>
+// Konsolen Applikation mit Menü
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace ModernConsoleTemplate
+{
+    /* Imports from NET Framework */
+    using System;
+
+    /* Imports from ModernUI Framework */
+    using ModernConsole.Menu;
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            SmartMenu.Menu("Console Template")
+                .Add("Menüpunkt 1, Obere Ebene", () => { MenuPoint1("Menüpunkt 1, Obere Ebene"); })
+
+              .Add(SmartMenu.Menu("Menüpunkt 2, Obere Ebene")
+                .Add("Menüpunkt 1, Zweite Ebene", () => { MenuPoint2("Menüpunkt 1, Zweite Ebene"); },1)
+                .Add("Menüpunkt 2, Zweite Ebene", () => { MenuPoint2("Menüpunkt 2, Zweite Ebene"); },1)
+                ).Show();
+
+        }
+
+        private static void ApplicationExit()
+        {
+            Environment.Exit(0);
+        }
+
+        private static void MenuPoint1(string ebene)
+        {
+            MConsole.Clear();
+
+            MConsole.Alert(ebene, "Menüpunkt", ModernConsole.Message.ConsoleMessageType.Info);
+
+            MConsole.Wait();
+        }
+
+        private static void MenuPoint2(string ebene)
+        {
+            MConsole.Clear();
+
+            MConsole.Alert(ebene,"Menüpunkt",ModernConsole.Message.ConsoleMessageType.Info);
+
+            MConsole.Wait();
+        }
+    }
+}
---
+++ ModernConsoleTemplate/README.md
@@ -0,0 +1,8 @@
+# $safeprojectname$
+
+![NET](https://img.shields.io/badge/NET-8.0-green.svg)
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+![VS2022](https://img.shields.io/badge/Visual%20Studio-2022-white.svg)
+![Version](https://img.shields.io/badge/Version-1.0.2025.0-yellow.svg)]
+
+Projekt Template für einen Window Konsolenprogramm
